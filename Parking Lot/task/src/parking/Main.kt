296325a49dcf73b0e0d6420846488a12298650c3
29olya <zorinavlolga@gmail.com>
2023-06-13 19:38:22 +0300

    stage 4

--- Parking Lot/task/src/parking/Main.kt
+++ Parking Lot/task/src/parking/Main.kt
@@ -35,12 +35,16 @@ class ParkingLot(
 }
 
 fun main() {
-
     val listOfCreatedSpots = mutableListOf<ParkingLot>()
 
-    for (i in 0 until 20) {
-        val spot = ParkingLot().createSpot(true)
-        listOfCreatedSpots.add(spot)
+    fun createParkingLot(input: String) {
+        listOfCreatedSpots.clear()
+        val spotsNumber = input.split(" ")[1].toInt()
+        for (i in 0 until spotsNumber) {
+            val spot = ParkingLot().createSpot(true)
+            listOfCreatedSpots.add(spot)
+        }
+        println("Created a parking lot with $spotsNumber spots.")
     }
 
     fun checkIsFull(): Boolean {
@@ -53,32 +57,73 @@ fun main() {
         return occupiedSpots == listOfCreatedSpots.size
     }
 
-    while (true) {
-        val inputString = readln()
-        when {
-            inputString.startsWith("park") -> {
-                if (checkIsFull()) {
-                    println("Sorry, the parking lot is full.")
-                } else {
-                    for (i in 0 until listOfCreatedSpots.size) {
-                        if (!listOfCreatedSpots[i].isFree) {
-                            continue
-                        } else {
-                            listOfCreatedSpots[i].park(inputString, i + 1)
-                            break
-                        }
+    fun checkIsEmpty(): Boolean {
+        var emptySpots = 0
+        for (i in 0 until listOfCreatedSpots.size) {
+            if (listOfCreatedSpots[i].isFree) {
+                emptySpots ++
+            }
+        }
+        return emptySpots == listOfCreatedSpots.size
+    }
+
+    fun park(input: String) {
+        if (listOfCreatedSpots.isEmpty()) {
+            println("Sorry, a parking lot has not been created.")
+        } else {
+            if (checkIsFull()) {
+                println("Sorry, the parking lot is full.")
+            } else {
+                for (i in 0 until listOfCreatedSpots.size) {
+                    if (!listOfCreatedSpots[i].isFree) {
+                        continue
+                    } else {
+                        listOfCreatedSpots[i].park(input, i + 1)
+                        break
                     }
                 }
-
-            }
-            inputString.startsWith("leave") -> {
-                val numberOfSpot = inputString.split(" ")[1].toInt()
-                listOfCreatedSpots[numberOfSpot - 1].leave(numberOfSpot)
-            }
-            inputString == "exit" -> {
-                exitProcess(1)
             }
         }
     }
+
+    fun leave(input: String) {
+        if (listOfCreatedSpots.isEmpty()) {
+            println("Sorry, a parking lot has not been created.")
+        } else {
+            val numberOfSpot = input.split(" ")[1].toInt()
+            listOfCreatedSpots[numberOfSpot - 1].leave(numberOfSpot)
+        }
+    }
+
+    fun status() {
+        if (listOfCreatedSpots.isEmpty()) {
+            println("Sorry, a parking lot has not been created.")
+        } else {
+            if (checkIsEmpty()) {
+                println("Parking lot is empty.")
+            } else {
+                for (i in 0 until listOfCreatedSpots.size) {
+                    if (listOfCreatedSpots[i].isFree) continue
+                    println("${i + 1} ${listOfCreatedSpots[i].regNumber} ${listOfCreatedSpots[i].colorOfTheCar}")
+                }
+            }
+        }
+    }
+
+    fun exit() {
+        exitProcess(1)
+    }
+
+    while (true) {
+        val inputString = readln()
+        when {
+            inputString.startsWith("create") -> createParkingLot(inputString)
+            inputString.startsWith("park") -> park(inputString)
+            inputString.startsWith("leave") -> leave(inputString)
+            inputString == "status" -> status()
+            inputString == "exit" -> exit()
+        }
+    }
 }
+
 
